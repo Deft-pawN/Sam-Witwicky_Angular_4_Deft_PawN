495cae00ce8a8eda934e71c51974eb5d978a4f27
Deft-pawN <799397253@qq.com>
2017-12-26 08:56:49 +0000

    20171226

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component'; 
 import { Code404Component } from './code404/code404.component'; 
 import { ProductDescComponent } from './product-desc/product-desc.component'; 
-import { SellerInfoComponent } from './seller-info/seller-info.component'; 
+import { SellerInfoComponent } from './seller-info/seller-info.component';
+import { ChatComponent } from './chat/chat.component'; 
 const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'product/:id',component:ProductComponent,children:[
-    {path:'',component:ProductDescComponent},
-    {path:'seller/:id',component:SellerInfoComponent},
+    
+  {path:'product-desc',component:ProductDescComponent},
+  {path:'seller/:id',component:SellerInfoComponent},
+  
     ]},
+  {path:'chat',component:ChatComponent,outlet:'aux'},
   {path:'**',component:Code404Component},
   //注意通配符的路由需要放在最后一个，否则就会首先匹配
   //path 不能用/开头
